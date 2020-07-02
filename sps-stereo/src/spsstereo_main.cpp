cf40501f4067f865f9da42cba72d3c897d6d0066
Yaoyu Hu <yaoyuh@andrew.cmu.edu>
2020-07-02 14:21:29 -0400

    More utility files to a new package stereo_utils.

--- sps-stereo/src/spsstereo_main.cpp
+++ sps-stereo/src/spsstereo_main.cpp
@@ -22,20 +22,24 @@
 #include <string>
 #include <vector>
 
+// This is needed because I am using the latest Eigen.
+// Otherwise, the compiler will complain about type name `eigen`
+// when #include <opencv2/core/eigen.hpp> is parsed.
 #include <Eigen/Dense>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/eigen.hpp>
 
+#include <stereo_utils/stereo_utils.hpp>
+#include <stereo_utils/Filesystem.hpp>
+#include <stereo_utils/JSONHelper.hpp>
+#include <stereo_utils/MatrixIO.hpp>
+#include <stereo_utils/NumPyIO.hpp>
+#include <stereo_utils/PLY/PLY.hpp>
+#include <stereo_utils/SimpleTime.hpp>
+
 #include "SPSStereo.h"
 #include "defParameter.h"
-
-#include "Filesystem.hpp"
-#include "JSONHelper.hpp"
-#include "MatrixIO.hpp"
-#include "NumPyIO.hpp"
-#include "PLY/PLY.hpp"
-#include "SimpleTime.hpp"
 
 using JSON = nlohmann::json;
 
@@ -190,142 +194,6 @@ void writeBoundaryLabelFile(const std::vector< std::vector<int> >& boundaryLabel
 		outputFileStream << boundaryLabels[boundaryIndex][2] << std::endl;
 	}
 	outputFileStream.close();
-}
-
-void convert_eigen_matrix_2_mat( 
-	const Eigen::MatrixXf &e, 
-	cv::Mat &m ) {
-	cv::eigen2cv( e, m );
-}
-
-void load_true_disparity(const std::string &fn, cv::Mat &trueDisp) {
-	Eigen::MatrixXf eTrueDisp;
-	read_npy_2_eigen_matrix(fn, eTrueDisp);
-	convert_eigen_matrix_2_mat( eTrueDisp, trueDisp );
-}
-
-struct DiffStat {
-	float mean;
-	float minVal;
-	float maxVal;
-};
-
-static DiffStat compute_diff_statistics( 
-	const cv::Mat &diff, const cv::Mat &mask=cv::Mat() ) {
-
-	if ( 1 != diff.channels() ) {
-		throw std::runtime_error("diff must be 1 channel. ");
-	}
-
-	float acc      = 0.f;
-	int   n        = 0;
-
-	DiffStat ds;
-	ds.mean   = 0.f;
-	ds.minVal = diff.cols * 2;
-	ds.maxVal = 0.f;
-
-	if ( !mask.empty() ) {
-		assert( diff.rows == mask.rows );
-		assert( diff.cols == mask.cols );
-
-		for ( int i = 0; i < diff.rows; ++i ) {
-			const float *pd   = diff.ptr<float>(i);
-			const uint8_t *pm = mask.ptr<uint8_t>(i);
-			for ( int j = 0; j < diff.cols; ++j ) {
-				const uint8_t m = pm[j];
-				if ( m > 0 ) {
-					const float d = pd[j];
-
-					if ( d > ds.maxVal ) {
-						ds.maxVal = d;
-					}
-
-					if ( d < ds.minVal ) {
-						ds.minVal = d;
-					}
-
-					acc += d;
-					n++;
-				}
-			}
-		}
-
-		if ( n == 0 ) {
-			n = 1;
-		}
-
-		ds.mean = acc / n;
-	} else {
-		for ( int i = 0; i < diff.rows; ++i ) {
-			const float *pd   = diff.ptr<float>(i);
-			for ( int j = 0; j < diff.cols; ++j ) {
-				const float d = pd[j];
-
-				if ( d > ds.maxVal ) {
-					ds.maxVal = d;
-				}
-
-				if ( d < ds.minVal ) {
-					ds.minVal = d;
-				}
-
-				acc += d;
-				n++;
-			}
-		}
-
-		if ( n == 0 ) {
-			n = 1;
-		}
-
-		ds.mean = acc / n;
-	}
-
-	return ds;
-}
-
-void compare_with_true_disparity( 
-	const cv::Mat &trueDisp, 
-	const cv::Mat &predDisp, 
-	cv::Mat &diff ) {
-
-	cv::Mat maskFloat;
-	cv::threshold( trueDisp, maskFloat, 0, 255, cv::THRESH_BINARY);
-
-	cv::Mat mask;
-	maskFloat.convertTo(mask, CV_8UC1);
-
-	std::cout << "mask.type() = " << mask.type() << std::endl;
-	
-	// It is assumed that the data type of trueDisp and 
-	// predDisp is floating point.
-	diff = cv::abs( trueDisp - predDisp );
-	
-	DiffStat ds = compute_diff_statistics( diff, mask );
-
-	std::cout << "diff mean = " << ds.mean << ", "
-	          << "diff min = "  << ds.minVal << ", "
-			  << "diff max = "  << ds.maxVal << ". \n";
-}
-
-void save_float_image_self_normalize( 
-	const std::string fn, 
-	const cv::Mat &img ) {
-	
-	const int type = img.type() % 8;
-	assert( type == 5 || type == 6 );
-	assert( 1 == img.channels() );
-
-	test_directory_by_filename(fn);
-
-	// Normalize the image.
-	double minVal, maxVal;
-	cv::minMaxLoc(img, &minVal, &maxVal);
-
-	cv::Mat normalized = (img - minVal) / ( maxVal - minVal ) * 255;
-
-	cv::imwrite(fn, normalized);
 }
 
 void process( const CaseDescription &cd ) {
---
+++ stereo_utils/include/stereo_utils/stereo_utils.hpp
@@ -0,0 +1,40 @@
+
+#ifndef __STEREO_UTILS__STEREO_UTILS_HPP__
+#define __STEREO_UTILS__STEREO_UTILS_HPP__
+
+#include <string>
+
+#include <Eigen/Dense>
+
+#include <opencv2/opencv.hpp>
+
+#include "stereo_utils.hpp"
+
+void convert_eigen_matrix_2_mat( 
+	const Eigen::MatrixXf &e, 
+	cv::Mat &m );
+
+void load_true_disparity(
+    const std::string &fn, 
+    cv::Mat &trueDisp);
+
+struct DiffStat {
+	float mean;
+	float minVal;
+	float maxVal;
+};
+
+DiffStat compute_diff_statistics( 
+	const cv::Mat &diff, 
+    const cv::Mat &mask=cv::Mat() );
+
+void compare_with_true_disparity( 
+	const cv::Mat &trueDisp, 
+	const cv::Mat &predDisp, 
+	cv::Mat &diff );
+
+void save_float_image_self_normalize( 
+	const std::string fn, 
+	const cv::Mat &img );
+
+#endif // __STEREO_UTILS__STEREO_UTILS_HPP__
