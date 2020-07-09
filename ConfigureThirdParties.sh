08d54bce5cae21c007635bc68f5e05948854be0b
Yaoyu Hu <yaoyuh@andrew.cmu.edu>
2020-07-09 00:21:39 -0400

    Fix CMakeLists.txt files.

--- ConfigureThirdParties.sh
+++ ConfigureThirdParties.sh
@@ -6,12 +6,18 @@ mkdir ThirdParties
 cd ThirdParties
 
 # Clone Eigen.
-git clone https://github.com/eigenteam/eigen-git-mirror.git eigen-src
+EIGEN_SRC=eigen-src
+if [ -d "${EIGEN_SRC}" ]
+then
+    rm -rf ${EIGEN_SRC}
+fi
+
+git clone https://github.com/eigenteam/eigen-git-mirror.git ${EIGEN_SRC}
 
 # Install Eigen to specific location.
 EIGEN_INSTALL_PREFIX=${PWD}/eigen-install
 mkdir ${EIGEN_INSTALL_PREFIX}
-cd eigen-src
+cd ${EIGEN_SRC}
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${EIGEN_INSTALL_PREFIX}
@@ -21,13 +27,18 @@ rm -rf build/
 cd ..
 
 # Clone cnpy.
-git clone https://github.com/rogersce/cnpy.git cnpy-src
+CNPY_SRC=cnpy-src
+if [ -d "${CNPY_SRC}" ]
+then
+    rm -rf ${CNPY_SRC}
+fi
+git clone https://github.com/rogersce/cnpy.git ${CNPY_SRC}
 
 # Install cnpy to specific location.
 CNPY_INSTALL_PREFIX=${PWD}/cnpy-install
 mkdir ${CNPY_INSTALL_PREFIX}
 cd https://github.com/rogersce/cnpy.git
-cd cnpy-src
+cd ${CNPY_SRC}
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${CNPY_INSTALL_PREFIX}
